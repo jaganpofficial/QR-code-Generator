b5071c492d98cf340ae03a16c2922c77e0b95d08
Jagan <jaganjnofficial@gmail.com>
2023-12-18 20:55:07 +0530

    added

---
+++ README.md
@@ -0,0 +1,5 @@
+# QR Code generator
+
+This QR Code Generator can make QR Code of any Text or Link. You will get the QR Code in Image format. We will make it using HTML CSS and JavaScript.
+
+QR Code API: https://goqr.me/api/
---
+++ script.js
@@ -0,0 +1,19 @@
+let imgBox = document.getElementById("img-box");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+let url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let btn = document.querySelector("button");
+
+
+function generateQR() {
+	if (qrText.value.length > 0) {
+		qrImg.src = url + qrText.value;
+		imgBox.classList.add("show-img");
+	} else {
+		qrText.classList.add("error");
+		setTimeout(() => {
+			qrText.classList.remove("error");
+		}, 1000);
+	}
+}
+btn.addEventListener("click", generateQR);
